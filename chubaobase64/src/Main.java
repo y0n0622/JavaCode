a7a803a24711124548d1eccc7648f66c7997b401
y0n0622 <469977511@qq.com>
2021-03-31 17:18:13 +0800

    Add files via upload

---
+++ chubaobase64/src/Main.java
@@ -0,0 +1,106 @@
+public class Main {
+//    private static final byte[] u = {110, -49, 71, -112, -10, 5, -3, -11, 3, -4, -5, 3, -11, -17, 17, -7, -1, 8, 3, 3, -1, -21, 9, -2, -15, 13, -1, -3, -17, 12, -14, 15, -9, -8, 13, -23, 0, 13, -2, 1, -17, 5, 43, -2, -5, 1, -84, 77, -14, -2, GLSquare.MIDDLE, -5, -15, -3, -70, 85, -17, -1, 1, -9, -71, 69, -4, 7, -11, 11, -19, 17, -13, 3, -9, -73, 81, -16, 12, -2, -12, 4, -3, -80, 71, -7, -56, -16, -5, 1, 10, -3, -2, -11, -4, 15, -17, -3, -38, 52, -15, -10, -35, 53, -16, -5, 1, 33, -6, 0, 10, -3, -80, 78, -17, 15, -1, -16, -45, -28, -2, -2, -2, -78, 30, -2, -34, 53, -2, -70, 38, -2, -16, 49, -2, -53, 55, -2, -3, -16, -27, 9, -11, -17, GLSquare.TOP, 33, -6, 0, 10, -3, -54, -28, 42, 4, -9, 4, -4, 5, -65, -28, -1, -7, 1, -15, -23, 23, -8, 15, -1, -5, -15, 12, -16, 3, -12, 9, 6, 0, 4, -9, 4, -17, -3, 11, -2, -5, 1, -84, 71, 3, -80, 81, -5, -82, 72, 7, -6, -3, 9, 1, -3, 68, 11, -12, 12, -5, -8, -7, -56, 64, -13, 11, -7, -6, 8, -2, -12, 4, -3, -66, 39, -7, -11, 9, -17, 24, -28, 17, -6, -17, 11, -6, -15, 9, 17, -14, -1, -7, 1, -19, 4, -4, 11, -2, -2, -2, -2, -2, 33, -2, -2, -2, -2, -2, 75, -9, -5, 1, -1, 6, -11, -71, -4, 9, -9, 7, -12, -5, 15, 10, -3, -2, -11, -4, 15, -13, 11, -15, 9, 3, -8, 4, -2, 11, -12, 12, -5, -8, -7, -56, 57, 3, 4, -17, 7, 4, -72, 49, 0, 15, -13, 4, -3, -66, 21, -3, 3, 9, -13, -9, -22, 2, -12, 0, 5};
+
+    public static void main(String[] args) {
+//        System.out.println(encrypt("endcard", "eb850c76efa59fa2"));
+//        System.out.println(encrypt("https://i.snssdk.com/api/ad/union/sdk/stats/", "eb850c76efa59fa2"));
+//        System.out.println(encrypt("https://is.snssdk.com/api/ad/union/sdk/get_ads/", "eb850c76efa59fa2"));
+//        System.out.println(encrypt("https://i.snssdk.com/", "eb850c76efa59fa2"));
+//        System.out.println(encrypt("get_ads", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("Bg1VG10MVUMRDw1GFwcPVhcNUVEeDlJSDAcVXFYIT0YRAVRaQwY=", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("EwtcUF88QkQJ", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("Bg1OUEI8QkQJ", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("Bg1OUEI8X1MMAQlB", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("Bg1OUEI8QF8BEgk=", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("FwdLWlwWQ18KCA==", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("FgtCUA==", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("EwtcUF88U0MXBxVcVgg=", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("FQ5ZTFEBW1M6Ag5CVwoOUwE9TUdc", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("AwtUUG8LVkUN", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("EwtcUF8=", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("Qw1KXFUNQ1cRDw5bBBYOQBEQWVxE", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("Qw1KXFUNQ1cRDw5bBBYOQBEQWVxE", "eb850c76efa59fa2"));
+//        System.out.println(decrypt("DRZMRUNZGBkMFU9GVxUSVg5MW1pdTFZGDEkAURYTD1sKDBdGVAgYUQASPlRdFU4=", "eb850c76efa59fa2"));
+
+//        byte[] decode = Base64.decode("aHR0cDovL2FwcG9ydGFsLmFpcnB1c2guY29tL2VwL3VybA==", 0);
+//        byte b2 = u[77];
+//        System.out.println(String(decode, a(b2, b2 | GLSquare.TOP, 129)));
+
+    }
+
+//    private static String a(int arg10, int arg11, int arg12) {
+//        int v0_1;
+//        int v1_1;
+//        byte[] v3_1;
+//        int v6;
+//        int v4;
+//        int v3 = arg12 + 4;
+//        int v0 = 0x79 - arg11;
+//        byte[] v8 = q.u;
+//        int v2 = arg10 + 1;
+//        byte[] v1 = new byte[v2];
+//        if(v8 == null) {
+//            v4 = 0;
+//            v6 = v3;
+//            v3_1 = v1;
+//            v1_1 = v2;
+//            v1_1 = v0 + v1_1 + 2;
+//            v0_1 = v4;
+//        }
+//        else {
+//            v6 = v3;
+//            v3_1 = v1;
+//            v1_1 = v0;
+//            v0_1 = 0;
+//        }
+//
+//        while (true){
+//            //label_18:
+//            v4 = v0_1 + 1;
+//            v3_1[v0_1] = (byte)v1_1;
+//            ++v6;
+//            if(v4 != v2) {
+//                v0 = v8[v6];
+//                v1_1 = v0 + v1_1 + 2;
+//                v0_1 = v4;
+////            goto label_18;
+//
+//            }
+//
+//        }
+//
+//        return new String(v3_1, 0).intern();
+//    }
+
+    public static String decrypt(String str, String str2) {
+        try {
+            return new String(xor(Base64.decode(str, 2), str2), "UTF-8");
+        } catch (Exception unused) {
+            return new String(xor(Base64.decode(str, 2), str2));
+        }
+    }
+    private static byte[] xor(byte[] bArr, String str) {
+        int length = bArr.length;
+        int length2 = str.length();
+        int i = 0;
+        int i2 = 0;
+        while (i < length) {
+            if (i2 >= length2) {
+                i2 = 0;
+            }
+            bArr[i] = (byte) (bArr[i] ^ str.charAt(i2));
+            i++;
+            i2++;
+        }
+        return bArr;
+    }
+    public static String encrypt(String str, String str2) {
+        try {
+            return new String(Base64.encode(xor(str.getBytes("UTF-8"), str2), 2));
+        } catch (Exception unused) {
+            return new String(Base64.encode(xor(str.getBytes(), str2), 2));
+        }
+    }
+
+
+}
