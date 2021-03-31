ea61d880aa8682c9bcc54d305b9dab6e0b5d0160
y0n0622 <469977511@qq.com>
2021-03-31 17:29:42 +0800

    Add files via upload

---
+++ toutiaojiemi/src/AESUtil.java
@@ -0,0 +1,22 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AESUtil {
+    public static String b(String arg3, String arg4) {
+        if(arg3.isEmpty()) {
+            return null;
+        }
+
+        try {
+            byte[] v3_1 = Base64.decode(arg3, 0);
+            SecretKeySpec v0 = new SecretKeySpec(arg4.getBytes(), "AES");
+            Cipher v4 = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            v4.init(2, v0);
+            return new String(v4.doFinal(v3_1));
+        }
+        catch(Exception v3) {
+            v3.printStackTrace();
+            return null;
+        }
+    }
+}
